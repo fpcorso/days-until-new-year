cfec7d38fb6d10341d406b8e77412fe558aab1eb
Frank Corso <frank@frankcorso.me>
2021-12-11 15:04:11 -0500

    Create method for getting our secrets from parameter store

--- lambda_function.py
+++ lambda_function.py
@@ -1,3 +1,20 @@
+import boto3
+
+
 def lambda_handler(event, context):
     """Main Lambda function"""
     return
+
+
+def get_twitter_keys():
+    """Retrieve secrets from Parameter Store."""
+    # Create our SSM Client.
+    aws_client = boto3.client('ssm')
+
+    # Get our parameter
+    response = aws_client.get_parameter(
+        Name='example_secret',
+        WithDecryption=True
+    )
+
+    return response['Parameter']['Value']
