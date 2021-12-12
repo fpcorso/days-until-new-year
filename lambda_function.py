4f03dd6b34b2b6a1a09fecc19e1002b722972505
Frank Corso <frank@frankcorso.me>
2021-12-12 15:06:40 -0500

    Updates parameter names with valid names

--- lambda_function.py
+++ lambda_function.py
@@ -21,11 +21,14 @@ def lambda_handler(event, context):
 
 def get_tweet():
     """Creates our tweet."""
+
+    # Calculate days until new year's day.
     next_year = datetime.datetime.now().year + 1
     new_year = datetime.datetime(next_year, 1, 1, 0, 0, 0)
     today = datetime.datetime.combine(datetime.date.today(), datetime.datetime.min.time())
     days_left = (new_year - today).days
 
+    # Customize text based on the number of days until the new year.
     if days_left == 1:
         tweet = "Tomorrow is New Year's Day!"
     elif today.month == 1 and today.day == 1:
@@ -41,10 +44,13 @@ def get_twitter_keys():
     # Create our SSM Client.
     aws_client = boto3.client('ssm')
 
-    # Get our keys.
+    # Get our keys from Parameter Store.
     parameters = aws_client.get_parameters(
         Names=[
-            'example_secret'
+            'twitter_api_key'
+            'twitter_api_secret',
+            'twitter_access_token',
+            'twitter_access_secret'
         ],
         WithDecryption=True
     )
@@ -54,8 +60,15 @@ def get_twitter_keys():
     access_token = ''
     access_secret = ''
 
+    # Cycle over the returned parameters and set the right value to each variable.
     for parameter in parameters['Parameters']:
-        if parameter['Name'] == '':
+        if parameter['Name'] == 'twitter_api_key':
             api_key = parameter['Value']
+        if parameter['Name'] == 'twitter_api_secret':
+            api_secret = parameter['Value']
+        if parameter['Name'] == 'twitter_access_token':
+            access_token = parameter['Value']
+        if parameter['Name'] == 'twitter_access_secret':
+            access_secret = parameter['Value']
 
     return api_key, api_secret, access_token, access_secret
